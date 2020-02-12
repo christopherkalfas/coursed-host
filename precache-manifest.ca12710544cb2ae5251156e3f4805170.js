be52bebf6ef3290512165680d4d2e5f9851d58fe
Chris Kalfas <christopherkalfas22@gmail.com>
2020-02-12 18:18:05 -0500

    Updates

---
+++ precache-manifest.ca12710544cb2ae5251156e3f4805170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfe368818addc9379dfc93774ad98bb",
+    "url": "/coursed-host/index.html"
+  },
+  {
+    "revision": "f8cb11dfff38ce7530fe",
+    "url": "/coursed-host/static/css/main.ceca84a1.chunk.css"
+  },
+  {
+    "revision": "bfbece7f30032d0e40e4",
+    "url": "/coursed-host/static/js/2.088b2e9c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/coursed-host/static/js/2.088b2e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8cb11dfff38ce7530fe",
+    "url": "/coursed-host/static/js/main.333f5e62.chunk.js"
+  },
+  {
+    "revision": "ce27d753f5c4d0077fdb",
+    "url": "/coursed-host/static/js/runtime-main.a63316c3.js"
+  }
+]);
